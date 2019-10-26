6a54d67992f32a4e5b2ce32c0a0e05a80b6ae78c
Valentine Maillard <valentinemaillard1@gmail.com>
2019-10-26 15:58:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,9 @@ end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-  :name => "Grace Hopper"
+  pioneer = {
+    :name => "Grace Hopper"
+}
 end
 
 def id_generator
