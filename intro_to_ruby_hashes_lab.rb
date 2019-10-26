f4d8a7ddb2bcd5d42cd64162ade7f444a94b3a30
Valentine Maillard <valentinemaillard1@gmail.com>
2019-10-26 16:03:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ end
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
   my_hash_creator = {
-    :key => value
+    :key => "value"
   }
 end
 
